f1aba51801055fbe277009bf48666ac6dd57da2f
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-09-22 18:05:43 -0300

    refactor(Character.ts): Changed attack method's parameter. Changed it from Fighter to SimpleFighter. Now all types of Fighters that implement SimpleFighter can be attacked by Character.

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import { Energy } from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -37,7 +37,7 @@ class Character implements Fighter {
   get dexterity() { return this._dexterity; }
   get energy() { return { ...this._energy }; }
 
-  attack(enemy: Fighter): void {
+  attack(enemy: SimpleFighter): void {
     enemy.receiveDamage(this._strength);
   }
 
