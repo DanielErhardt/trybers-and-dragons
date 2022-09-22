0d631e424c393cfc33fc8b178c7a7e4b973c2fc3
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-09-22 18:06:00 -0300

    feat: Created and implemented Monster class as requested by the evaluator.

---
+++ src/Monster.ts
@@ -0,0 +1,28 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 83;
+  }
+
+  get lifePoints() { return this._lifePoints; }
+  get strength() { return this._strength; }
+  
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  receiveDamage(attackPoints: number): number {
+    this._lifePoints -= attackPoints;
+
+    if (this._lifePoints <= 0) this._lifePoints = -1;
+
+    return this._lifePoints;
+  }
+}
+
+export default Monster;
