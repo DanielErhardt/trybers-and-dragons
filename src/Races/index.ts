e9fb8ea774ae32e69b234dd8de29372d4233bf0c
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-09-22 15:54:22 -0300

    feat: Moved all imports to Races/index.ts

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,3 +1,9 @@
 import Race from './Race';
+import Orc from './Orc';
+import Dwarf from './Dwarf';
+import Elf from './Elf';
+import Halfling from './Halfling';
+
+export { Orc, Dwarf, Elf, Halfling };
 
 export default Race;
