a501a9bd052a2dd44821d2fcc63ef7f2e6068fdf
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-09-22 16:13:40 -0300

    feat: Created abstract class Archetypes.

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,28 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+  protected static _instances = 0;
+
+  constructor(name: string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  get name() { return this._name; }
+
+  get special() { return this._special; }
+
+  get cost() { return this._cost; }
+
+  abstract get energyType(): EnergyType;
+
+  static createdArchetypeInstances(): number {
+    return Archetype._instances;
+  }
+}
+
+export default Archetype;
